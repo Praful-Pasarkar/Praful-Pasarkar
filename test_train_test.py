ce08d948e19cb8274628b0cb60032b467cd0cfe5
Manasi Abhyankar <manasia2@illinois.edu>
2021-06-12 23:42:21 +0400

    Fixed the test cases.

--- test_train_test.py
+++ test_train_test.py
@@ -1,16 +1,23 @@
 from unittest import TestCase
 
+import pytest
+
 from train_test import basic
 
 
 class Testbasic(TestCase):
     def test_input_check(self):
         result = basic.input_check(5, 10)
-        self.assertEqual(result, 5)
+        self.assertEqual(result, 5, 'They are equal')
 
     def test_input_csv_2(self):
-        result = basic.input_check(5, 10)
-        self.assertEqual(result, 5)
+        result = basic.input_csv_2(5, 10)
+        self.assertEqual(result, 5, 'They are equal')
 
     def test_input_csv(self):
-        self.assertEqual(basic.input_check(5), "Image CSV folder")
+        self.assertEqual(basic.input_csv(5), "Image CSV folder", 'They are equal')
+
+    def test_check(self):
+        result = basic.check(5)
+        b = 5
+        self.assertEqual(result, b, 'They are equal')
--- train_test.py
+++ train_test.py
@@ -1,10 +1,15 @@
 class basic :
     def input_check(img_type,img):
         print(img_type)
-
+        return img_type
 
     def input_csv_2(img_type, img_csv):
         print(img_type)
+        return img_type
 
     def input_csv(img_csv):
         print("Image CSV folder")
+        return "Image CSV folder"
+
+    def check(a):
+        return a
