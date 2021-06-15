ff69c0d35704d00f4ecfa820fb9ad80bc56d423c
Praful-Pasarkar <31927706+Praful-Pasarkar@users.noreply.github.com>
2021-06-15 11:36:26 +0100

    Update train_test.py
    Added comments and what we are doing.

--- train_test.py
+++ train_test.py
@@ -1,3 +1,12 @@
+# Basic class to read the arguments 
+# three  types of arguments:
+# 1. python3 train_test <train object name> abc.jpg
+# 2. python3 train_test <train object name> abc.jpg, pqr.jpg, poir.jpg  (look at the comma)
+# 3. python3 train_test <train object name> abc.jpg pqr.jpr poir.jpg    (look at the space can be 1+ spaces)
+# 4. python3 train_test abc.csv (Train object name is inside the csv file)
+# 5. python3 train_test <train object name> abc.csv
+# what happens when the <train object name> is not given?
+
 class basic :
     def input_check(img_type,img):
         print(img_type)
