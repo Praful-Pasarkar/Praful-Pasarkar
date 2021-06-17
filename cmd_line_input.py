2439cdb1ca25858fead7dfe63f0ae7fcf0bf65af
Manasi Abhyankar <manasia2@illinois.edu>
2021-06-17 14:05:42 +0400

    Fixed an error.

--- cmd_line_input.py
+++ cmd_line_input.py
@@ -1,48 +1,46 @@
 import csv
 import string
+import model
 
 
-class cmd_input:
+def is_csv(infile):
+    # Code is cited from stackoverflow
+    try:
+        with open(infile, newline='') as csvfile:
+            start = csvfile.read(4096)
 
-    def accept_input():
-        user_inp = input("Enter\n")
-        space = ' '
-        space_check = -1
-        word = '.img'
-        word_csv = '.csv'
-        x = 1  # default option assuming input is a folder of images
-        if space in user_inp:
-            space_check = 0
-        if (space_check == 0):
-            inp,inp1 = user_inp.split() #splitting user input
-            #print(inp)
-            #print(inp1)
+            if not all([c in string.printable or c.isprintable() for c in start]):
+                return False
+            dialect = csv.Sniffer().sniff(start)
+            return True
+    except csv.Error:
+        # Could not get a csv dialect -> probably not a csv.
+        return False
 
 
-            if is_csv(inp) == True:
-                x = 2  # if only 1 img provided as input
-            elif is_csv(inp1) == True:
-                x = 3  # if csv is provided as 2nd input
+class CmdInput:
+
+    user_inp = input("Enter\n")
+    space = ' '
+    space_check = -1
+    word = '.img'
+    word_csv = '.csv'
+    x = 1  # default option assuming input is a folder of images
+    if space in user_inp:
+        space_check = 0
+    if space_check == 0:
+        inp, inp1 = user_inp.split()  # splitting user input
+        if is_csv(inp):
+            data = list(csv.reader(open(inp)))
+            model.identify_image(data)  #sends a 2d list containing all the images to the model class
+        elif is_csv(inp1):
+            data = list(csv.reader(open(inp)))
+            model.identify_image(data)  #sends a 2d list containing all the images to the model class
+    else:
+        x = 0  # if no second input is provided
+        print("Error: invalid input")
+        if word_csv in user_inp:
+            x = 0  # only csv input
         else:
-            x = 0  # if no second input is provided
-            print("hi")
-            if word_csv in user_inp:
-                x = 0  # only csv input
-            else:
-                x = -1  # invalid input as no training images provided
-        #if(len(inp1) == 0):
-
-    def is_csv(infile):
-
-        #Code is cited from stackoverflow
-        try:
-            with open(infile, newline='') as csvfile:
-                start = csvfile.read(4096)
-
-                if not all([c in string.printable or c.isprintable() for c in start]):
-                    return False
-                dialect = csv.Sniffer().sniff(start)
-                return True
-        except csv.Error:
-            # Could not get a csv dialect -> probably not a csv.
-            return False
+            x = -1  # invalid input as no training images provided
+    # if(len(inp1) == 0):
