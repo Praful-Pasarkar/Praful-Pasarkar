1d27b5c62fc0896dd79875b4be30a288f470c161
Mihir-cs <abhyankm@purdue.edu>
2021-06-18 11:41:09 +0400

    oracle db insert

--- oracle_db_insert_class.py
+++ oracle_db_insert_class.py
@@ -1,13 +1,19 @@
 import cx_Oracle
 
 class oracle_insert:
-    print("test")
+    print('Enter CUSTOMER Data values')
+    id = input('ID:\n')
+    name = input('Name:\n')
+    age = input('Age:\n')
+    add = input('Address\n')
+    sal = input('Salary\n')
     connection = cx_Oracle.connect('praful', 'A1_34_intern','almltrdb_low')
     mysql1 = """select * from CUSTOMERS order by ID"""
-    mysql2 = """INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) VALUES (10, 'K', 21, 'MQ', 400 )"""
+    mysql2 = """INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) VALUES (?, ?, ?, ?, ? )"""
+    data = (id, name, age, add, sal);
     print("Get all rows")
     cursor = connection.cursor()
-    cursor.execute(mysql2)
+    cursor.execute(mysql2, data)
     for result in cursor.execute(mysql1):
         print(result)
     print()
