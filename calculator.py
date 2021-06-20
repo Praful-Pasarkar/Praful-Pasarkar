04346ed705ef8c57ac4249da13c7801ab21c3c4f
Manasi Abhyankar <manasia2@illinois.edu>
2021-06-20 19:57:15 +0400

    Updated the calculator class.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 # This function adds two numbers
 def add(x, y):
     return x + y
@@ -18,12 +21,67 @@ def divide(x, y):
     return x / y
 
 
+def simple_calculations(num1, num2, choice):
+    # Calls the appropriate function and stores the result in a list
+    if choice == '1':
+        result = add(num1, num2)
+        previous_results.append(result)
+        print(result)
+
+    elif choice == '2':
+        result = subtract(num1, num2)
+        previous_results.append(result)
+        print(result)
+
+    elif choice == '3':
+        result = multiply(num1, num2)
+        previous_results.append(result)
+        print(result)
+
+    elif choice == '4':
+        result = divide(num1, num2)
+        previous_results.append(result)
+        print(result)
+
+
+def scientific_calculations(num1, choice):
+    if choice == '6':
+        type = input('Choose sin/cos/tan : s/c/t')
+        if type == 's':
+            result = np.sin(num1)
+            previous_results.append(result)
+            print(result)
+
+        elif type == 'c':
+            result = np.cos(num1)
+            previous_results.append(result)
+            print(result)
+
+        elif type == 't':
+            result = np.tan(num1)
+            previous_results.append(result)
+            print(result)
+
+    elif choice == '7':
+        result = num1 ** 2
+        previous_results.append(result)
+        print(result)
+
+    elif choice == '8':
+        result = np.sqrt(num1)
+        previous_results.append(result)
+        print(result)
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.Display saved results")
+print("6.Calculate Angle")
+print("7.Square")
+print("8.Square Root")
 
 # Stores the last 5 results
 previous_results = []
@@ -33,37 +91,23 @@ continue_calculating = True
 while continue_calculating:
 
     # Take input from the user
-    choice = input("Enter choice(1/2/3/4/5): ")
+    choice = input("Enter choice(1/2/3/4/5/6/7/8): ")
 
     # Check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        # Calls the appropriate function and stores the result in a list
-        if choice == '1':
-            result = add(num1, num2)
-            previous_results.append(result)
-            print(result)
-
-        elif choice == '2':
-            result = subtract(num1, num2)
-            previous_results.append(result)
-            print(result)
-
-        elif choice == '3':
-            result = multiply(num1, num2)
-            previous_results.append(result)
-            print(result)
-
-        elif choice == '4':
-            result = divide(num1, num2)
-            previous_results.append(result)
-            print(result)
+        simple_calculations(num1, num2, choice)
 
     # Prints the list
     elif choice == '5':
         print(previous_results)
+
+    elif choice in ('6', '7', '8'):
+        num1 = float(input("Enter a number: "))
+
+        scientific_calculations(num1, choice)
 
     else:
         print("Invalid Input")
