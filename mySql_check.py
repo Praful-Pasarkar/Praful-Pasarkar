66b25caf11709d940ac9e16e65e2857abebae47b
Mihir-cs <abhyankm@purdue.edu>
2021-06-21 22:56:53 +0400

    push

--- mySql_check.py
+++ mySql_check.py
@@ -4,6 +4,7 @@ config = {
     'user': 'root',
     'password': 'A1_34_intern',
     'host': '193.123.68.57',
+    'database' : 'books',
     'client_flags': [ClientFlag.SSL],
     'ssl_ca': 'C:\project_Downloads\ca.pem',
     'ssl_cert': 'C:\project_Downloads\client-cert.pem',
@@ -11,8 +12,15 @@ config = {
 }
 try:
     cnx = mysql.connector.connect(**config)
+    #cnx = mysql.connector.connect(user = 'root', password = 'A1_34_intern', host = '193.123.68.57',
+    #client_flags = [ClientFlag.SSL],
+    #ssl_ca ='C:\project_Downloads\ca.pem',
+    #ssl_cert = 'C:\project_Downloads\client-cert.pem',
+    #ssl_key = 'C:\project_Downloads\client-key.pem')
     cur = cnx.cursor(buffered=True)
-    cur.execute("SELECT count() FROM DUAL")
+    #cur.execute("SELECT count() FROM DUAL")
+    #cur.execute("SHOW STATUS LIKE 'Ssl_cipher'")
+    cur.execute("SELECT * FROM authors")
     print(cur.fetchone())
     cur.close()
     cnx.close()
