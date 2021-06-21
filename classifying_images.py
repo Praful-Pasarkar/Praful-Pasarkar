0006f3f28caa43bc284b6db4a03f11b3cf8be008
Manasi Abhyankar <manasia2@illinois.edu>
2021-06-21 11:08:30 +0400

    Updated cmd_line_input.py

--- classifying_images.py
+++ classifying_images.py
@@ -7,30 +7,32 @@
 # 5. python3 train_test <train object name> abc.csv
 # what happens when the <train object name> is not given?
 
+
+# File for reference: Input will be taken from cmd_line_input.py
+
 import csv
 import accuracy_checker
 
+
 class classifying_images:
 
-    #imput is: image type and image
-    def input_check(img_type,img):
+    # input is: image type and image
+    def input_check(img_type, img):
         print(img_type)
         accuracy_checker.accuracy(img, img_type)
         return img_type
 
-    #imput is: csv file
+    # input is: csv file
     def input_csv(img_csv):
         with open(img_csv, newline='') as f:
             reader = csv.reader(f)
             image_type = next(reader)  # gets the first line
 
-        accuracy_checker.accuracy('image', image_type)  #is each line a new image in the csv file?
+        accuracy_checker.accuracy('image', image_type)  # is each line a new image in the csv file?
         return image_type
 
-    # imput is: csv file and image type
+    # input is: image type and csv file
     def input_csv_2(img_type, img_csv):
         print(img_type)
         accuracy_checker.accuracy(img_type)
         return img_type
-
-
--- cmd_line_input.py
+++ cmd_line_input.py
@@ -1,8 +1,14 @@
 import csv
 import string
+
+import filetype
+from numpy import genfromtxt
+from PIL import Image
+
+import accuracy_checker
 import model
 
-
+# Checks if the file is a csv file
 def is_csv(infile):
     # Code is cited from stackoverflow
     try:
@@ -17,30 +23,48 @@ def is_csv(infile):
         # Could not get a csv dialect -> probably not a csv.
         return False
 
+# Checks if the input is a string
+def is_string(inp):
+    res = isinstance(inp, str)
+    if str(res):
+        return True
+    else:
+        return False
+
+# Checks if the input is an image
+def is_image(filename):
+    if filetype.is_image(filename):
+        return True
+    else:
+        return False
+
+# Converts a csv file to an image
+def convert_csv_to_img(file):
+    my_data = genfromtxt('my_file.csv', delimiter=',')
+    im = Image.fromarray(my_data)
+    return im
 
 class CmdInput:
-
     user_inp = input("Enter\n")
     space = ' '
-    space_check = -1
-    word = '.img'
-    word_csv = '.csv'
-    x = 1  # default option assuming input is a folder of images
     if space in user_inp:
-        space_check = 0
-    if space_check == 0:
         inp, inp1 = user_inp.split()  # splitting user input
-        if is_csv(inp):
-            data = list(csv.reader(open(inp)))
-            model.identify_image(data)  #sends a 2d list containing all the images to the model class
-        elif is_csv(inp1):
-            data = list(csv.reader(open(inp)))
-            model.identify_image(data)  #sends a 2d list containing all the images to the model class
+
+        # 1st case: inp is a the type of image and inp1 is an image
+        if is_string(inp) and is_image(inp1):
+            accuracy_checker.accuracy(inp1, inp)
+
+        # 2nd case: inp is a the type of image and inp1 is a csv file
+        elif is_string(inp) and is_csv(inp1):
+            image = convert_csv_to_img(inp1)
+            accuracy_checker.accuracy(image, inp)
+
+    # if no second input is provided
     else:
-        x = 0  # if no second input is provided
-        print("Error: invalid input")
-        if word_csv in user_inp:
-            x = 0  # only csv input
+        if is_csv(user_inp):
+            image = convert_csv_to_img(user_inp)
+            accuracy_checker.accuracy(image, user_inp)
         else:
-            x = -1  # invalid input as no training images provided
-    # if(len(inp1) == 0):
+            print("Error: invalid input")
+
+# Is the csv file one image or multiple images? Currently, it's one image.
