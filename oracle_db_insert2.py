79da3d8fea109bf2e026a8fa670d96c6f3c0dbfd
Mihir-cs <abhyankm@purdue.edu>
2021-06-22 12:50:11 +0400

    push

--- oracle_db_insert2.py
+++ oracle_db_insert2.py
@@ -7,17 +7,40 @@ class oracle_db_insert2:
         'Do something'
 
 
-    def create_connection(self):
+    def create_connection(username):
         'Code for creating connection to oracle db'
+        connection = cx_Oracle.connect(user=username, password="A1_34_intern", dsn="almltrdb_low", encoding="UTF-8")
+        cursor = connection.cursor()
+        return connection, cursor
 
-    def receive_my_input(self):
+    def receive_my_input(cursor):
         'To recieve the the input from outside class'
+        id1 = input("ID:\n")
+        name1 = input('Name:\n')
+        age1 = input('Age:\n')
+        add1 = input('Address\n')
+        sal1 = input('Salary\n')
+        return id1, name1, age1, add1, sal1
 
-    def insert_into_table(self):
+    def insert_into_table(cursor, id1, name1,age1, add1, sal1):
         'The is code to insert data'
+        mysql3 = f'INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) VALUES (:id1, :name1 , :age1, :add1, :sal1 )'
+        cursor.execute(mysql3, [id1, name1, age1, add1, sal1])
 
-    def print_my_result(self):
+    def print_my_result(connection, cursor):
         'Pretty-fy my result'
+        mysql1 = """select * from CUSTOMERS order by ID"""
+        for result in cursor.execute(mysql1):
+            print(result)
+        print()
+        connection.close()
 
-oracle_db_insert2('Send the values')
-oracle_db_insert2('Get a handle and call each def seperately to do the work')
+#oracle_db_insert2('Send the values')
+#oracle_db_insert2('Get a handle and call each def seperately to do the work')
+username = input("Enter user: praful/mihir/manasi\n")
+connection, cursor = oracle_db_insert2.create_connection(username)
+
+id1, name1, age1, add1, sal1 = oracle_db_insert2.receive_my_input(cursor)
+
+oracle_db_insert2.insert_into_table(cursor,id1, name1, age1, add1, sal1)
+oracle_db_insert2.print_my_result(connection, cursor)
