c1435ad258df7934c5ccfea2c5f7afe07b3c0bbc
Mihir Abh <abhyankm@purdue.edu>
2021-07-28 00:17:40 +0400

    implemented gui

--- test_models/test_cifar_save.py
+++ test_models/test_cifar_save.py
@@ -10,12 +10,14 @@ from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
 from PIL import Image
 import numpy as np
+import easygui
 from tensorflow.keras.models import load_model
-model = load_model('model_cifar_try1_e5.h5')
-results = ['aeroplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+model = load_model('model_cifar_final.h5')
+results = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 check = 'y'
 while check == 'y' or check == 'Y':
-    inp = input("Enter img path \n")
+    #inp = input("Enter img path \n")
+    inp = easygui.fileopenbox()
     im = Image.open(inp)
     im = im.resize((32, 32))
     im = np.expand_dims(im, axis=0)
@@ -24,3 +26,4 @@ while check == 'y' or check == 'Y':
     print(pred, ' \n')
     print(results[pred])
     check = input("Enter Y or N \n")
+#print(easygui.fileopenbox())
